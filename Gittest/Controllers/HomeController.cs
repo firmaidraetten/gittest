f2f5602d633d3612cc8d889d6a3a02f6355beaad
ts-firmaidraet <ts@firmaidraet.dk>
2015-11-27 10:27:14 +0100

    HomeController added

---
+++ Gittest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gittest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (1 == 2)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
